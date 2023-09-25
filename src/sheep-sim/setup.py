45b4d66b47b15e1a7d15d5483a9cb03ac5cd1106
yuval-dimri <juviyu@gmail.com>
2023-09-25 16:54:00 +0300

    the correct email

--- src/sheep-sim/setup.py
+++ src/sheep-sim/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='yuval',
-    maintainer_email='juviyu@gmail.com',
+    maintainer_email='yuval5363@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
