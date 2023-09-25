c904c6415dde95884e05f1c126ba606a7e97673f
yuval-dimri <juviyu@gmail.com>
2023-09-25 21:59:26 +0300

    basic simulation is running

--- src/sheep-sim/setup.py
+++ src/sheep-sim/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'sheep-sim'
 
@@ -9,7 +11,15 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+
+        (os.path.join('share', package_name, 'launch'), glob(
+            os.path.join('launch', '*launch.[pxy][yma]*')))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
