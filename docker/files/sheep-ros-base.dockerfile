5926ff9545ba33c8586f044d507ee9366db51610
yuval-dimri <juviyu@gmail.com>
2023-10-03 21:54:16 +0300

    many changes, idk

--- docker/files/sheep-ros-base.dockerfile
+++
@@ -1,2 +0,0 @@
-FROM osrf/ros2:humble
-
--- src/sheep-sim/launch/sim.launch.py
+++ src/sheep-sim/launch/sim.launch.py
@@ -1,73 +1,68 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
 import xacro
+
+pkg_path = os.path.join(
+    get_package_share_directory('sheep-sim'))
+
+robot_xacro_file_path = os.path.join(pkg_path, 'urdf', 'sheep_basic.xacro')
+
+
+gazebo_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([os.path.join(
+        get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+    launch_arguments={'world': 'worlds/willowgarage.world'}.items()
+)
+
+
+doc = xacro.parse(open(robot_xacro_file_path))
+xacro.process_doc(doc)
+rsp_params = {'robot_description': doc.toxml()}
+node_robot_state_publisher = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    output='screen',
+    parameters=[rsp_params]
+)
+
+node_spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                         arguments=['-topic', 'robot_description',
+                                    '-entity', 'cartpole'],
+                         output='screen')
+
+node_joint_state_controller = ExecuteProcess(
+    cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+         'joint_state_broadcaster'],
+    output='screen'
+)
+
+node_joint_trajectory_controller = ExecuteProcess(
+    cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+         'diff_drive_base_controller'],
+    output='screen'
+)
 
 
 def generate_launch_description():
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-    )
-
-    pkg_path = os.path.join(
-        get_package_share_directory('sheep-sim'))
-
-    xacro_file = os.path.join(pkg_path,
-                              'urdf',
-                              'sheep_basic.xacro')
-
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    params = {'robot_description': doc.toxml()}
-
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
-    )
-
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'cartpole'],
-                        output='screen')
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'diff_drive_base_controller'],
-        output='screen'
-    )
-
     return LaunchDescription([
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[load_joint_state_controller],
+                target_action=node_spawn_entity,
+                on_exit=[node_joint_state_controller],
             )
         ),
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=load_joint_state_controller,
-                on_exit=[load_joint_trajectory_controller],
+                target_action=node_joint_state_controller,
+                on_exit=[node_joint_trajectory_controller],
             )
         ),
-        gazebo,
+        gazebo_launch,
         node_robot_state_publisher,
-        spawn_entity,
+        node_spawn_entity,
     ])
